49bd9faa2c735dc683fdeb3a10f51c8fd33183c2
Semin Lee <seminlee@gmail.com>
2009-04-16 17:34:09 +0100

    Modified license info

--- pyzebra.py
+++ pyzebra.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python
 
-##    pyzebra - 
+##    pyzebra
 ##    Copyright (C) 2008-2011 Semin Lee
 ##
-##    This program is free software; you can redistribute it and/or
-##    modify it under the terms of the GNU Library General Public
-##    License as published by the Free Software Foundation; either
-##    version 2 of the License, or (at your option) any later version.
-##
-##    This library is distributed in the hope that it will be useful,
-##    but WITHOUT ANY WARRANTY; without even the implied warranty of
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-##    Library General Public License for more details.
-##
-##    You should have received a copy of the GNU Library General Public
-##    License along with this library; if not, write to the Free
-##    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-##
 ##    Semin Lee
-##    semin@cryst.bioc.cam.ac.uk
+##    seminlee at gmail dot com
 ##
 
 
